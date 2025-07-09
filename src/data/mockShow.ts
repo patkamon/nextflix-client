42bca625e0b0aeac22aed99d792f735c66d5da21
patkamon <patkamontura@gmail.com>
2025-07-09 21:19:32 +0700

    add mock data

---
+++ src/data/mockShow.ts
@@ -0,0 +1,89 @@
+import { SmallShowInterface } from "@/interface/SmallShowInterface";
+
+export const shows: SmallShowInterface[] = [
+    {
+      id: "1",
+      name: "Shadow Realm",
+      image: "images/MovieCard_1.svg",
+      description: "A tale of darkness and redemption.",
+      type: "tv",
+      status: "new",
+      isTop10: true,
+      isNetflixOriginal: true
+    },
+    {
+      id: "2",
+      name: "Future Drift",
+      image: "images/MovieCard_2.svg",
+      description: "High-speed racing in a cyberpunk world.",
+      type: "anime",
+      status: "new",
+      isTop10: false,
+      isNetflixOriginal: true
+    },
+    {
+      id: "3",
+      name: "Echoes of War",
+      image: "images/MovieCard_1.svg",
+      type: "movie",
+      status: "classic",
+      isTop10: true
+    },
+    {
+      id: "4",
+      name: "Skybound",
+      image: "images/MovieCard_2.svg",
+      description: "A journey across floating islands.",
+      type: "tv",
+      status: "popular"
+    },
+    {
+      id: "5",
+      name: "The Neon Path",
+      image: "images/MovieCard_1.svg",
+      description: "A hacker’s fight for justice.",
+      type: "anime",
+      status: "trending",
+      isNetflixOriginal: true
+    },
+    {
+      id: "6",
+      name: "Lost Horizon",
+      image: "images/MovieCard_2.svg",
+      type: "movie",
+      status: "upcoming"
+    },
+    {
+      id: "7",
+      name: "Mythos",
+      image: "images/MovieCard_2.svg",
+      description: "Ancient gods clash in modern times.",
+      type: "tv",
+      isTop10: true
+    },
+    {
+      id: "8",
+      name: "Crimson Tower",
+      image: "images/MovieCard_1.svg",
+      type: "anime",
+      status: "popular",
+      isTop10: true
+    },
+    {
+      id: "9",
+      name: "Silent Winter",
+      image: "images/MovieCard_1.svg",
+      description: "Survival in a post-apocalyptic frost.",
+      type: "movie"
+    },
+    {
+      id: "10",
+      name: "Galaxy Riders",
+      image: "images/MovieCard_2.svg",
+      description: "Space bounty hunters on the run.",
+      type: "tv",
+      status: "new",
+      isNetflixOriginal: true
+    }
+  ];
+  
